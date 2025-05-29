d979ab9f47b9cf1450bbc0bdc57bbcefceda28e4
Ansu D Nishad <ansu@elemensissoftech.com>
2025-05-29 10:37:24 +0530

    updated new data

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
     {
@@ -49,5 +50,42 @@ const userSchema = new Schema(
  
     {timestamps : true}
 )
+userSchema.pre("save" , async function (next){
+    //if the modifired field is not a password then return next
+     if (!this.modified("password")) return next()
+    //if the modifird field is password or you are updating the password then then run this code
+    this.password = bcrypt.hash(this.password, 10)
+
+
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password,this.password)
+}
+//it takes the user password and data base password and try to match them but the db password is encrypted and the user password is in normal text ..
+//bt we can convert that also in encrypted an match them but we dont need to match it manually
+//so we use bcrypt
+
+userSchema.methods.generateAccessToken = function (){
+  //short lived access token       
+  return jwt.sign({
+    _id : rhis._id,  //when we destructured my token i can grab this info
+    email : this.email,
+    username : this.username,
+    fukkkname : this.fullname
+  },process.env.ACCESS_TOKEN_SECRET,   //secret msg or info
+  {expiresIn : process.env.ACCESS_TOKEN_EXPIRY}
+)
+}
+
+userSchema.methods.generateRefreshToken = function (){
+  //long lived refresh token       
+  return jwt.sign({
+    _id : rhis._id,  //when we destructured my token i can grab this info
+  },process.env.REFRESH_TOKEN_SECRET,   //secret msg or info
+  {expiresIn : process.env.REFRESH_TOKEN_EXPIRY}
+)
+}
 
 export const User =mongoose.model("User" , userSchema)
